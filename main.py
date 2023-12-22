7fcea0e18e5dcd30da23e8a53d6aa54108e18226
Divide-By-0 <aayushg@mit.edu>
2023-12-22 05:24:28 -0500

    added raffi/simons changes to have it run on start

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
+from do_thing import start
 
 while True:
-    print("press enter key to draw art")
+    print("""---------------pen plotter---------------
+Press enter key to draw art of yourself.
+
+Smile for the camera! (photo is not stored).
+Use flashlight for best results.
+""", end="")
     test = input()
     try:
-        os.system("python3 do_thing.py | ./gcode-cli - /dev/tty,250000")
+        start()
+        os.system('gcode-cli one_line.gcode "/dev/ttyACM0,b250000"')
     except Exception as e:
         print("failed")
         print(e)
